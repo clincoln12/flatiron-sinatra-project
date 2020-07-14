7d9d391c19f1aed80d033bad1e6dab26473fe9e7
clincoln12 <chris.lincoln12@gmail.com>
2020-07-14 19:43:05 -0400

    deleted default welcome view and added new index. updated password to password_digest because of bcrypt. added sessions + secure password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,45 @@
 class UserController < ApplicationController
 
   get '/users/signup' do
-    erb :'/users/signup'
+    if logged_in?
+      redirect to '/recipes'
+    else
+      erb :'/users/signup'
+    end  
   end
 
   post '/users/signup' do
-    @user = User.create(username: params[:username], password: params[:password])
+    @user = User.create(
+      username: params[:username], 
+      password: params[:password]
+      )
+    session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
+  end
+
+  get '/users/login' do
+    erb :'/users/login'
+  end
+
+  post '/users/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:critic_id] = @user.id
+      redirect "/recipes"
+    else
+      redirect "/users/login"
+    end
   end
 
   get '/users/:id' do
     @user = User.find(params[:id])
     erb :'/users/show'
   end
+
+  get '/users/logout' do
+    session.clear
+    redirect to '/users/login'
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200714002512) do
+ActiveRecord::Schema.define(version: 20200714175059) do
 
   create_table "recipes", force: :cascade do |t|
     t.string  "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20200714002512) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
